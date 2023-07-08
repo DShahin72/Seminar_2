c9363bb6089b6bc518fc9bb70df5767f6160013e
Dmitry Shakhin <dshahin@yandex.ru>
2023-07-08 19:48:31 +0300

    Проверка кратности двух чисел

---
+++ Task 12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и проверять, является ли
+// второе число кратным первому. Если нет, то выводить остаток от деления.
+
+int M = new Random().Next(1, 100);
+int N = new Random().Next(1, 100);
+System.Console.WriteLine(M);
+System.Console.WriteLine(N);
+
+if (N % M == 0)
+{
+    System.Console.WriteLine($"Число {N} кратно числу {M}");
+}
+else
+{
+    System.Console.WriteLine($"Число {N} некратно числу {M}. Остаток от деления равен {N % M}");
+}
